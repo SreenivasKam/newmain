897de240130a23c6745ec6efd43b60378a0a99a6
Sreenivas <sreenivasmsa@gmail.com>
2022-05-06 00:35:01 +0530

    05-05-2022 all are fine hopefully not a mess

--- projectfolder/demand/views.py
+++ projectfolder/demand/views.py
@@ -21,7 +21,7 @@ def demander():
     header = (cur.fetchall())
     cur.execute("SELECT * from demands;")
     data = list(cur.fetchall())
-    return render_template('demand.html', user=header, data=data)
+    return render_template('demand.html', user=header, data=data, filter=0)
 
 
 @demand.route('/add_data')
@@ -56,40 +56,40 @@ def filter():
     return render_template('filter.html', count=count)
 
 
-@demand.route('/projectfil', methods=['GET', 'POST'])
-def projectfil():
+@demand.route('/demandfil', methods=['GET', 'POST'])
+def demandfil():
     msg = 'Ok'
     cur = mysql.connection.cursor()
     cur.execute(
-       "SELECT * FROM legend where type_ in ('status','primary_jrss','position_type')")
+        "SELECT * FROM legend where type_ in ('status','primary_jrss','position_type')")
     count = list(cur.fetchall())
     t = ['New']
     cur.execute(
         "SHOW COLUMNS FROM demands;")
     header = (cur.fetchall())
-    g=' '
-    dict1 ={}
+    g = ' '
+    dict1 = {}
     if request.method == 'POST':
         for i in range(len(count)):
             d = str(count[i][0])
             s = str(count[i][1])
-            if(request.form.get(s, False)!=False):
+            if(request.form.get(s, False) != False):
                 if(d not in dict1.keys()):
                     dict1[d] = []
                 dict1[d].append(request.form.get(s, False))
         cur = mysql.connection.cursor()
-        p='select * from demands where'
+        p = 'select * from demands where'
         for i in dict1.keys():
             k = str((dict1[i]))
             k = k[1:]
             k = k[:-1]
-            p=p + " "+ str(i) + ' in (' + k +") or"
+            p = p + " " + str(i) + ' in (' + k + ") or"
         p = p[:-2]
         p = p+";"
         cur.execute(p)
         count1 = list(cur.fetchall())
-    #return render_template('check.html',msg = p)
-    return render_template('demand.html', user=header, data=count1)
+    # return render_template('check.html',msg = p)
+    return render_template('demand.html', user=header, data=count1, filter=1)
 
 
 @demand.route('/writedemand', methods=['GET', 'POST'])
@@ -102,12 +102,16 @@ def writedemand():
     # applying empty validation
     dict1 = {}
     t = []
+    m = []
     nothis = ['record_creation_date', 'last_updated_date', 'last_updated_by']
+    logs = ['id', 'status', 'sub_status']
     if request.method == 'POST':
         # passing HTML form data into python variable
         g = ''
         for i in range(len(header)-3):
             s = str(header[i][0]).lower()
+            if(s in logs):
+                m.append(request.form.get(s, False))
             t.append(request.form.get(s, False))
             g = g+'%s,'
             dict1[header[i][0].lower()] = request.form.get(s, False)
@@ -117,11 +121,133 @@ def writedemand():
             now = datetime.now()
             dt_string = now.strftime("%Y-%m-%d")
             t.append(dt_string)
+        comments = 'Created new Job demand.'
+        current_time = datetime.now()
+        m.append(comments)
+        m.append(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+        m = str(m)
+        m = m[1:-1]
         t = t[:-1]
         t.append("Sreenivas")
         g = g[:len(g)-1]
+        o = 'INSERT INTO data_logs VALUES (' + m + ');'
         cur.execute('INSERT INTO demands VALUES ('+g+')', tuple(t))
+        cur.execute(o)
         mysql.connection.commit()
         # displaying message
         msg = 'Data has been added to the database'
-    return render_template('check.html', msg=msg)
+        link = '/'
+    return render_template('check.html', msg=msg, link=link)
+
+
+@demand.route('/logs')
+def logs():
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SHOW COLUMNS FROM data_logs;")
+    header = (cur.fetchall())
+    cur.execute("SELECT * from data_logs;")
+    data = list(cur.fetchall())
+    cur.execute(
+        "SHOW COLUMNS FROM resume_logs;")
+    header1 = (cur.fetchall())
+    cur.execute("SELECT * from resume_logs;")
+    data1 = list(cur.fetchall())
+    return render_template('logs.html', user=header, data=data, user1=header1, data1=data1)
+
+
+@demand.route('/updates/<id>')
+def updates(id):
+    cur = mysql.connection.cursor()
+    p = "Select * from demands where ID ='"+id+"';"
+    cur.execute(p)
+    sendata = list(cur.fetchall())
+    nothis = ['record_creation_date', 'last_updated_date', 'last_updated_by']
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SHOW COLUMNS FROM demands;")
+    header = list(cur.fetchall())
+    cur.execute(
+        "Select * FROM demand_comments;")
+    verify = list(cur.fetchall())
+    cur.execute(
+        "SELECT DISTINCT(type_) FROM legend")
+    count = list(cur.fetchall())
+    cur.execute(
+        "SELECT * FROM legend")
+    tabledata = list(cur.fetchall())
+    data = []
+    for ele in count:
+        data.append(ele[0])
+    #return render_template('check.html',msg = sendata)
+    return render_template('updating.html',msg=sendata,verify=list(verify), user=header, count=data, tabledata=tabledata, nothis=nothis)
+
+@demand.route('/changes',methods=['GET', 'POST'])
+def changes():
+    msg = ' '
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SHOW COLUMNS FROM demands;")
+    header = list(cur.fetchall())
+    # applying empty validation
+    dict1 = {}
+    t = []
+    m = []
+    nothis = ['record_creation_date', 'last_updated_date', 'last_updated_by']
+    logs = ['id', 'status', 'sub_status']
+    if request.method == 'POST':
+        # passing HTML form data into python variable
+        p = 'update demands set '
+        for i in range(len(header)-3):
+            s = str(header[i][0]).lower()
+            if(i==0):
+                id=str(request.form.get(s, False))
+            else:
+                p = str(p)+ s + " = '" + str(request.form.get(s, False))+ "', "
+            if(s in logs):
+                m.append(request.form.get(s, False))
+
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d")
+        t.append(dt_string)
+        p = p+" last_updated_date = '" + dt_string + "', "
+        p = p+" last_updated_by = 'Sreenivas' where ID = '" + id +"' ;"
+
+        comments = request.form.get("update_comments", False)
+        current_time = datetime.now()
+        m.append(comments)
+        m.append(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+        m = str(m)
+        m = m[1:-1]
+        o = 'INSERT INTO data_logs VALUES (' + m + ');'
+        cur.execute(p)
+        cur.execute(o)
+        mysql.connection.commit()
+        # displaying message
+        msg = 'Data has been updated successfully'
+        link = '/'
+    return render_template('check.html', msg=msg,link=link)
+    #return render_template('check.html',msg = 'this works bro')
+
+@demand.route('/delete/<id>')
+def delete(id):
+    f = "delete from data_logs where unique_id = '" + id + "';"
+    p = "delete from demands where ID = '" + id + "';"
+    cur = mysql.connection.cursor()
+    current_time = datetime.now()
+    new = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    id = "Removed ID: " + id
+    m= ['admin','Deleted',id]
+    comments = "This data has been exhausted"
+    current_time = datetime.now()
+    m.append(comments)
+    m.append(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+    m = str(m)
+    m = m[1:-1]
+    o = 'INSERT INTO data_logs VALUES (' + m + ');'
+    cur.execute(f)
+    cur.execute(o)
+    cur.execute(p)
+    mysql.connection.commit()
+    msg = " This Record is successfully deleted"
+    return render_template("check.html",msg= msg)
--- projectfolder/resume/views.py
+++ projectfolder/resume/views.py
@@ -11,9 +11,12 @@ resume = Blueprint('resume', __name__)
 def resumer():
     cur = mysql.connection.cursor()
     cur.execute(
-        "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'emp_profiles';")
+        "SHOW COLUMNS FROM emp_profiles;")
     header = list(cur.fetchall())
-    return render_template('resume.html', user=header)
+    cur.execute(
+        "Select * FROM emp_profiles;")
+    data = list(cur.fetchall())
+    return render_template('resume.html', user=header,data=data,filter=0)
 
 
 @resume.route('/add_data_resume')
@@ -21,20 +24,96 @@ def add_data_resume():
     nothis= ['record_creation','last_updated_date','last_updated_by']
     cur = mysql.connection.cursor()
     cur.execute(
-        "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'emp_profiles';")
+        "SHOW COLUMNS FROM emp_profiles;")
     header = list(cur.fetchall())
     cur.execute(
-        "SELECT DISTINCT(type_) FROM legend")
-    count = list(cur.fetchall())
+        "Select * FROM  resume_comments;")
+    verify = list(cur.fetchall())
     cur.execute(
-        "SELECT * FROM legend")
+        "Select * FROM  hiring_legends;")
     tabledata = list(cur.fetchall())
-    data =[]
-    for ele in count:
-        data.append(ele[0])
-    return render_template('add_data_resume.html', user=header,count=data,tabledata = tabledata,nothis=nothis)
+    count = ['cdo_tower_status']
+    return render_template('add_data_resume.html', verify=list(verify),user=header,count=count,tabledata = tabledata,nothis=nothis)
     #return render_template('check.html',msg=nothis,count=data)
 
 @resume.route('/writeresume', methods=['GET','POST'])
 def writeresume():
-    pass
+    msg = ' '
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SHOW COLUMNS FROM emp_profiles;")
+    header = list(cur.fetchall())
+    # applying empty validation
+    dict1 = {}
+    t = []
+    m = []
+    nothis = ['record_creation_date', 'last_updated_date', 'last_updated_by']
+    logs =['unique_id','cdo_tower_status']
+    if request.method == 'POST':
+        # passing HTML form data into python variable
+        g = ''
+        for i in range(len(header)-3):
+            s = str(header[i][0]).lower()
+            t.append(request.form.get(s, False))
+            if(s in logs):
+                m.append(request.form.get(s, False))
+            g = g+'%s,'
+            dict1[header[i][0].lower()] = request.form.get(s, False)
+
+        for ele in nothis:
+            g = g+'%s,'
+            now = datetime.now()
+            dt_string = now.strftime("%Y-%m-%d")
+            t.append(dt_string)
+        t = t[:-1]
+        comments = 'Created new Resume added.'
+        current_time = datetime.now()  
+        m.append(comments)
+        m.append(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+        m = str(m)
+        m = m[1:-1]
+        t.append("Sreenivas")
+        g = g[:len(g)-1]
+        o = 'INSERT INTO resume_logs VALUES (' + m +');'
+        cur.execute('INSERT INTO emp_profiles VALUES ('+g+')', tuple(t))
+        cur.execute(o)
+        mysql.connection.commit()
+        # displaying message
+        msg = 'Data has been added to the database'
+        link='/resume'
+    return render_template('check.html', msg=msg,link=link)
+
+
+@resume.route('/filter_resume')
+def filter_resume():
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SELECT * FROM hiring_legends ;")
+    count = list(cur.fetchall())
+    return render_template("filter_resume.html",count=count)
+
+@resume.route('/resumefil', methods=['GET', 'POST'])
+def resumefil():
+    msg = 'Ok'
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SHOW COLUMNS FROM emp_profiles;")
+    header = list(cur.fetchall())
+    cur.execute(
+        "SELECT value_ FROM hiring_legends;")
+    count = list(cur.fetchall())
+    t=[]
+    dict1 ={}
+    if request.method == 'POST':
+        for i in range(len(count)):
+            s = str(count[i][0])
+            if(request.form.get(s, False)!=False):
+               t.append(request.form.get(s, False))
+        cur = mysql.connection.cursor()
+        b = str(t)
+        b = b[1:-1]
+        p='select * from emp_profiles where cdo_tower_status in (' + b+ ');'
+        cur.execute(p)
+        count1 = list(cur.fetchall())
+    #return render_template('check.html',msg = p)
+    return render_template('resume.html', user=header, data=count1,filter=1)
